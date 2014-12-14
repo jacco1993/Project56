f3db0d2a95a1b907955f8710f9b385c690a0286e
Joost de Ruijter <joost1235@hotmail.com>
2014-12-14 00:51:14 +0000

    Webservice

---
+++ WcfService2/wcfservice2/IService1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+using MongoDB;
+using MongoDB.Bson.Serialization.Attributes;
+using System.IO;
+
+namespace WcfService2
+{
+
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebGet(UriTemplate="getproducts")]
+        String getProducts();
+        [WebGet(UriTemplate = "getmusic", ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<Muziek> getMusic();
+        //MongoServer Server();
+    }
+    }
---
+++ WcfService2/wcfservice2/Service1.svc.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+using MongoDB;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.IO;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO;
+
+
+
+namespace WcfService2
+{
+    public class Muziek
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        public String artiest { get; set; }
+        public String genre { get; set; }
+        public Song songs { get; set; }
+    }
+
+
+
+    public class Song
+    {
+        public String title { get; set; }
+        public String title1 { get; set; }
+        public String title2 { get; set; }
+    }
+    
+    public class Products
+    {
+        public String title { get; set; }
+        public String title1 { get; set; }
+        public String title2 { get; set; }
+    }
+
+
+    public class Service1 : IService1
+    {
+
+        /*public MongoServer server;
+
+        public MongoServer Server()
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            return server;
+        }*/
+
+        public List<Muziek> getMusic()
+        {
+            var connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            MongoDatabase database = server.GetDatabase("DevOpdr");
+            MongoCollection<Muziek> muziek = database.GetCollection<Muziek>("Muziek");
+            MongoCursor<Muziek> alles = muziek.FindAll();
+            List<Muziek> muziekLijst = alles.ToList();
+            return muziekLijst;
+            //var jsonSerialiser = new JavaScriptSerializer();
+            //var json = jsonSerialiser.Serialize(muziekLijst);
+            //return new MemoryStream(Encoding.UTF8.GetBytes(json));
+            
+
+            //return json;
+        }
+
+        public String getProducts()
+        {
+            //Connect met MongoDB op de server
+            var connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            //Pak database "Project56", en de collectie "Products"
+            MongoDatabase database = server.GetDatabase("Project56");
+            MongoCollection<Products> products = database.GetCollection<Products>("Products");
+            //Pak alle BSON documenten
+            MongoCursor<Products> allProducts = products.FindAll();
+            //Zet alle documenten in een lijst
+            List<Products> productsList = allProducts.ToList();
+            //Zet BSON om in JSON
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(productsList);
+            //Geef JSON terug
+            return json.ToString();
+        }
+
+    }
+    
+}
