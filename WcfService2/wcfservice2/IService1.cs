b18717430b21513e39744ff26be41a289b45e3bd
Joost de Ruijter <joost1235@hotmail.com>
2014-12-14 00:56:43 +0000

    Webservice

--- WcfService2/wcfservice2/IService1.cs
+++ WcfService2/wcfservice2/IService1.cs
@@ -12,29 +12,20 @@ using MongoDB.Driver.GridFS;
 using MongoDB.Driver.Linq;
 using MongoDB;
 using MongoDB.Bson.Serialization.Attributes;
-<<<<<<< HEAD
 using System.IO;
-=======
->>>>>>> 69cfd330a9dec5e03f1e855adf8d95fb279eb001
 
 namespace WcfService2
 {
-
+    //
     [ServiceContract]
     public interface IService1
     {
         [OperationContract]
-<<<<<<< HEAD
         [WebGet(UriTemplate="getproducts")]
         String getProducts();
         [WebGet(UriTemplate = "getmusic", ResponseFormat = WebMessageFormat.Json)]
         [OperationContract]
         List<Muziek> getMusic();
-=======
-        String getProducts();
-        [OperationContract]
-        String getMusic();
->>>>>>> 69cfd330a9dec5e03f1e855adf8d95fb279eb001
         //MongoServer Server();
     }
     }
--- WcfService2/wcfservice2/Service1.svc.cs
+++ WcfService2/wcfservice2/Service1.svc.cs
@@ -16,10 +16,7 @@ using MongoDB.Bson.IO;
 using System.Web.Script.Serialization;
 using Newtonsoft.Json;
 using System.Diagnostics;
-<<<<<<< HEAD
 using System.IO;
-=======
->>>>>>> 69cfd330a9dec5e03f1e855adf8d95fb279eb001
 
 
 
@@ -27,28 +24,21 @@ namespace WcfService2
 {
     public class Muziek
     {
-<<<<<<< HEAD
         [BsonId]
-=======
->>>>>>> 69cfd330a9dec5e03f1e855adf8d95fb279eb001
-        public ObjectId _id { get; set; }
+        public ObjectId _id  { get; set; }
         public String artiest { get; set; }
         public String genre { get; set; }
         public Song songs { get; set; }
     }
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 69cfd330a9dec5e03f1e855adf8d95fb279eb001
     public class Song
     {
         public String title { get; set; }
         public String title1 { get; set; }
         public String title2 { get; set; }
     }
-<<<<<<< HEAD
     
     public class Products
     {
@@ -56,15 +46,11 @@ namespace WcfService2
         public String title1 { get; set; }
         public String title2 { get; set; }
     }
-=======
-
->>>>>>> 69cfd330a9dec5e03f1e855adf8d95fb279eb001
 
 
     public class Service1 : IService1
     {
 
-<<<<<<< HEAD
         /*public MongoServer server;
 
         public MongoServer Server()
@@ -76,9 +62,6 @@ namespace WcfService2
         }*/
 
         public List<Muziek> getMusic()
-=======
-        public String getProducts()
->>>>>>> 69cfd330a9dec5e03f1e855adf8d95fb279eb001
         {
             var connectionString = "mongodb://localhost";
             var client = new MongoClient(connectionString);
@@ -86,7 +69,6 @@ namespace WcfService2
             MongoDatabase database = server.GetDatabase("DevOpdr");
             MongoCollection<Muziek> muziek = database.GetCollection<Muziek>("Muziek");
             MongoCursor<Muziek> alles = muziek.FindAll();
-<<<<<<< HEAD
             List<Muziek> muziekLijst = alles.ToList();
             return muziekLijst;
             //var jsonSerialiser = new JavaScriptSerializer();
@@ -119,18 +101,4 @@ namespace WcfService2
 
     }
     
-=======
-            List<Muziek> lijst = alles.ToList();
-            var jsonSerialiser = new JavaScriptSerializer();
-            var json = jsonSerialiser.Serialize(lijst);
-            Debug.WriteLine(json.ToString());
-           return json.ToString();
-        }
-
-        public String getMusic()
-        {
-            return "Hoi!";
-        }
-    }
->>>>>>> 69cfd330a9dec5e03f1e855adf8d95fb279eb001
 }
