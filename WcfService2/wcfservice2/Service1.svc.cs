c1166e5178fab72ea73a6e5084e70a256224081a
Joost de Ruijter <joost1235@hotmail.com>
2014-12-17 13:27:40 +0000

    Removed unused methods/added comments

--- WcfService2/wcfservice2/Service1.svc.cs
+++ WcfService2/wcfservice2/Service1.svc.cs
@@ -22,24 +22,6 @@ using System.IO;
 
 namespace WcfService2
 {
-    public class Muziek
-    {
-        [BsonId]
-        public ObjectId _id  { get; set; }
-        public String artiest { get; set; }
-        public String genre { get; set; }
-        public Song songs { get; set; }
-    }
-
-
-
-    public class Song
-    {
-        public String title { get; set; }
-        public String title1 { get; set; }
-        public String title2 { get; set; }
-    }
-
     public class Products
     {
         public ObjectId _id { get; set; }
@@ -56,42 +38,13 @@ namespace WcfService2
 
     public class Service1 : IService1
     {
-
-        /*public MongoServer server;
-
-        public MongoServer Server()
-        {
-            var connectionString = "mongodb://localhost:27017";
-            var client = new MongoClient(connectionString);
-            var server = client.GetServer();
-            return server;
-        }*/
-
-        public List<Muziek> getMusic()
-        {
-            var connectionString = "mongodb://localhost";
-            var client = new MongoClient(connectionString);
-            var server = client.GetServer();
-            MongoDatabase database = server.GetDatabase("DevOpdr");
-            MongoCollection<Muziek> muziek = database.GetCollection<Muziek>("Muziek");
-            MongoCursor<Muziek> alles = muziek.FindAll();
-            List<Muziek> muziekLijst = alles.ToList();
-            return muziekLijst;
-            //var jsonSerialiser = new JavaScriptSerializer();
-            //var json = jsonSerialiser.Serialize(muziekLijst);
-            //return new MemoryStream(Encoding.UTF8.GetBytes(json));
-            
-
-            //return json;
-        }
-
         public List<Products> getProducts()
         {
             //Connect met MongoDB op de server
             var connectionString = "mongodb://localhost";
             var client = new MongoClient(connectionString);
             var server = client.GetServer();
-            //Pak database "Project56", en de collectie "Products"
+            //Pak database "Products", en de collectie "files2", hierin staan de producten
             MongoDatabase database = server.GetDatabase("Products");
             MongoCollection<Products> products = database.GetCollection<Products>("files2");
             //Pak alle BSON documenten
