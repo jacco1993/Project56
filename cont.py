3f76ec00d6ca3ab1cbfe534eb7c25195fc38c5aa
bbriann123 <brian_van_den_heuvel@me.com>
2014-12-23 22:02:47 +0100

    Continously crawling data Everyday at 1AM

--- cont.py
+++ cont.py
@@ -1,8 +1,28 @@
 import os
-i = 0
+import datetime
+import sys
+from apscheduler.scheduler import Scheduler
+import logging
+logging.basicConfig()
+
+today = str(datetime.date.today())
 turn = "false"
-while i <= 11:
-	os.system("python crawl.py %s %s" %(i, turn))
-	i+=1
-	if i == 10:
-		turn = "true"
+
+def run_crawler():
+	i = 0
+	while i <= 11:
+		global turn
+		os.system("python crawl.py %s %s" %(i, turn))
+		i+=1
+		if today != str(datetime.date.today()):
+			turn = "true"
+
+if __name__ == '__main__':
+	scheduler = Scheduler(standalone=True)
+	scheduler.add_cron_job(run_crawler,day_of_week='mon-sun', hour=1)
+	print("Running daily!")
+	print("Press CTRL+C anytime to quit")
+	try:
+		scheduler.start()
+	except (KeyboardInterrupt, SystemExit):
+		pass
