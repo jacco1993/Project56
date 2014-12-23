fe1ef1f1e18627ccb909b0666f4834805bd0f610
bbriann123 <brian_van_den_heuvel@me.com>
2014-12-23 22:34:28 +0100

    changed order of datecheck

--- cont.py
+++ cont.py
@@ -12,14 +12,14 @@ def run_crawler():
 	i = 0
 	while i <= 11:
 		global turn
-		os.system("python crawl.py %s %s" %(i, turn))
-		i+=1
 		if today != str(datetime.date.today()):
 			turn = "true"
+		os.system("python crawl.py %s %s" %(i, turn))
+		i+=1
 
 if __name__ == '__main__':
 	scheduler = Scheduler(standalone=True)
-	scheduler.add_cron_job(run_crawler,day_of_week='mon-sun', hour=1)
+	scheduler.add_cron_job(run_crawler,day_of_week='mon-sun', hour=22, minute=33)
 	print("Running daily!")
 	print("Press CTRL+C anytime to quit")
 	try:
