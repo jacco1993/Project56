262fb40270087674539b28d8138f7dadeabaa45a
bbriann123 <brian_van_den_heuvel@me.com>
2015-01-09 12:20:04 +0100

    website in java

---
+++ project56/WebContent/hello.php
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=US-ASCII"
+    pageEncoding="US-ASCII"%>
+    <%@page import="java.io.*" %>
+    <%@page import="java.net.*" %>
+    <%@page import="java.lang.*" %>
+    <%@page import="java.lang.*" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jstl/core" %>
+<%@ taglib prefix="x" uri="http://java.sun.com/jstl/xml" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
+<%@ taglib prefix="sql" uri="http://java.sun.com/jstl/sql" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head><title> Hello</title></head>
+<body><?php echo "hello php"; ?></body>
+</html>
